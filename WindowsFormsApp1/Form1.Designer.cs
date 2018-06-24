7bfe78746209416c6082ac176a371311e872d086
smloua3065 <smloua3065@gmail>
2018-06-24 21:19:14 +0800

    initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(616, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "順時針旋轉90度";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(616, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "逆時針旋轉90度";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(616, 241);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 60);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "灰階";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(616, 341);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 60);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "二值化";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(31, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(616, 441);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(127, 60);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "重設";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 537);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "圖片編輯";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //順時鐘90度
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile(@"../../../Lena.jpg");
+            Bitmap rotate = new Bitmap(image);
+            rotate = RotateBitmap(rotate, 90);
+            pictureBox2.Image = rotate;
+        }
+
+        //逆時鐘90度
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile(@"../../../Lena.jpg");
+            Bitmap rotate = new Bitmap(image);
+            rotate = RotateBitmap(rotate, 270);
+            pictureBox2.Image = rotate;
+        }
+
+        //灰階
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile(@"../../../Lena.jpg");
+            Bitmap gray = new Bitmap(image);
+            gray = doGray(gray);
+            pictureBox2.Image = gray;
+        }
+
+        //二值化
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile(@"../../../Lena.jpg");
+            Bitmap bpp = new Bitmap(image);
+            bpp = ConvertTo1Bpp1(bpp);
+            pictureBox2.Image = bpp;
+        }
+        
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //重設
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image= Image.FromFile(@"../../../Lena.jpg");
+        }
+
+
+        //-----------------------------------------以下函式-----------------------------------------------------
+       
+        //計算旋轉後影像Size
+        public Size CalculateNewSize(int width, int height, double RotateAngle)
+        {
+            double r = Math.Sqrt(Math.Pow((double)width / 2d, 2d) + Math.Pow((double)height / 2d, 2d)); //半徑L
+            double OriginalAngle = Math.Acos((width / 2d) / r) / Math.PI * 180d;  //對角線和X軸的角度θ
+            double minW = 0d, maxW = 0d, minH = 0d, maxH = 0d; //最大和最小的 X、Y座標
+            double[] drawPoint = new double[4];
+
+            drawPoint[0] = (-OriginalAngle + RotateAngle) * Math.PI / 180d;
+            drawPoint[1] = (OriginalAngle + RotateAngle) * Math.PI / 180d;
+            drawPoint[2] = (180f - OriginalAngle + RotateAngle) * Math.PI / 180d;
+            drawPoint[3] = (180f + OriginalAngle + RotateAngle) * Math.PI / 180d;
+
+            foreach (double point in drawPoint) //由四個角的點算出X、Y的最大值及最小值
+            {
+                double x = r * Math.Cos(point);
+                double y = r * Math.Sin(point);
+
+                if (x < minW)
+                    minW = x;
+                if (x > maxW)
+                    maxW = x;
+                if (y < minH)
+                    minH = y;
+                if (y > maxH)
+                    maxH = y;
+            }
+
+            return new Size((int)(maxW - minW), (int)(maxH - minH));
+        }
+
+
+        //灰階前置
+        public int[,,] getRGBData(Bitmap abimage)
+        {
+            // Step 1: 利用 Bitmap 將 image 包起來
+            int Height = abimage.Height;
+            int Width = abimage.Width;
+            int[,,] rgbData = new int[Width, Height, 3];
+
+            // Step 2: 取得像點顏色資訊
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Color color = abimage.GetPixel(x, y);
+                    rgbData[x, y, 0] = color.R;
+                    rgbData[x, y, 1] = color.G;
+                    rgbData[x, y, 2] = color.B;
+                }
+            }
+
+            return rgbData;
+        }
+
+        //旋轉圖片之函式
+        //參數    image：要旋轉的圖片  RotateAngle：旋轉角度
+        public Bitmap RotateBitmap(Bitmap image, float RotateAngle)
+        {
+            Size newSize = CalculateNewSize(image.Width, image.Height, RotateAngle);
+            Bitmap rotatedBmp = new Bitmap(newSize.Width, newSize.Height);
+            PointF centerPoint = new PointF((float)rotatedBmp.Width / 2f, (float)rotatedBmp.Height / 2f);
+            Graphics g = Graphics.FromImage(rotatedBmp);
+
+            g.TranslateTransform(centerPoint.X, centerPoint.Y);
+            g.RotateTransform(RotateAngle);
+            g.TranslateTransform(-centerPoint.X, -centerPoint.Y);
+
+            g.DrawImage(image, (float)(newSize.Width - image.Width) / 2f, (float)(newSize.Height - image.Height) / 2f, image.Width, image.Height);
+            g.Dispose();
+
+            return rotatedBmp;
+        }
+
+        //將圖片灰階之函式
+        // 參數    image:要灰階的圖片
+        public Bitmap doGray(Bitmap image)
+        {
+            // Step 1: 建立 Bitmap 元件
+            int[,,] rgbData = getRGBData(image);
+            int Height = image.Height;
+            int Width = image.Width;
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    int gray = (rgbData[x, y, 0] + rgbData[x, y, 1] + rgbData[x, y, 2]) / 3;
+                    image.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            return image;
+        }
+
+        //將圖片二值化之函式
+        //參數 bmp:要二值化的圖片
+        public static Bitmap ConvertTo1Bpp1(Bitmap bmp)
+        {
+            int average = 0;      
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color color = bmp.GetPixel(i, j);
+                    average += color.B;
+                }
+            }
+            average = average / (bmp.Width * bmp.Height); 
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    //獲取該點的像素的RGB的顏色  
+                    Color color = bmp.GetPixel(i, j);
+                    int value = 255 - color.B;
+                    Color newColor = value > average ? Color.FromArgb(0, 0, 0) : Color.FromArgb(255, 255, 255);
+                    bmp.SetPixel(i, j, newColor);
+                }
+            }
+            return bmp;
+        }
+
+    }
+}
